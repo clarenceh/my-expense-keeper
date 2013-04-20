4a13c4cc080d1c07af734eecf32a96f11e1e5ad3
Clarence Ho <clarence@skywidesoft.com>
2013-04-20 15:58:53 +0800

    Minor changes on listing page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myExpenseKeeperApp', [])
+angular.module('myExpenseKeeperApp', ['ui', 'ui.bootstrap'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
