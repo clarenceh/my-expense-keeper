9c7201e69093de9fa6ef3694d459e0683776c8a7
Clarence Ho <clarence@skywidesoft.com>
2013-04-24 15:12:37 +0800

    Initial version of add expense form

--- app/scripts/controllers/expenseview.js
+++ app/scripts/controllers/expenseview.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('myExpenseKeeperApp')
-  .controller('ExpenseviewCtrl', function ($scope, $http, $routeParams, $log, $location) {
+  .controller('ExpenseviewCtrl', function ($scope, $http, $routeParams, $log, $location, $dialog) {
 
         $scope.action = 'View';
 
@@ -20,6 +20,20 @@ angular.module('myExpenseKeeperApp')
             $location.path('/expenseedit/' + expenseId);
         }
 
+        $scope.confirmDelete = function() {
+            var title = 'Confirm Deletion';
+            var msg = 'Are you sure to delete this expense item?';
+            var btns = [{result:'cancel', label: 'Cancel'}, {result:'delete', label: 'Delete', cssClass: 'btn-danger'}];
+
+            $dialog.messageBox(title, msg, btns)
+                .open()
+                .then(function(result){
+                    if (result === 'delete') {
+                        $scope.delete();
+                    }
+                });
+        }
+
         $scope.delete = function() {
             $log.info('Deleting expense');
 
@@ -28,7 +42,7 @@ angular.module('myExpenseKeeperApp')
                 $log.info('Delete expense success!');
 
                 // Redirect to list page
-                $location.path('/expenselist');
+                $location.path('/expenselist').replace();
             }).error(function(data, status) {
                     $log.info('Save expense fail: ' + status);
             });
