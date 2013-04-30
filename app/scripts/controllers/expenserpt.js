2b87325b3274f46425014b96f6d45cd5b516e035
Clarence Ho <clarence@skywidesoft.com>
2013-04-30 15:36:06 +0800

    Initial files for expense report function

--- app/scripts/controllers/expenserpt.js
+++ app/scripts/controllers/expenserpt.js
@@ -1,14 +1,26 @@
 'use strict';
 
 angular.module('myExpenseKeeperApp')
-  .controller('ExpenserptCtrl', function ($scope, $location) {
+  .controller('ExpenserptCtrl', function ($scope, $location, $log) {
 
         $scope.rptCriteria = {};
 
         $scope.groupByOptions = ['Category', 'Location'];
 
+        $scope.showRpt = false;
+
         $scope.cancel = function() {
             $location.path('/expenselist');
+        }
+
+        $scope.renderRpt = function() {
+
+            // Retrieve data from server for display report
+            $log.info('Report options: from date: ' + $scope.rptCriteria.fromDate + ' to date: ' + $scope.rptCriteria.toDate + ' group by: ' + $scope.rptCriteria.groupBy);
+
+
+
+            $scope.showRpt = true;
         }
 
   });
--- app/scripts/directives/skychart.js
+++ app/scripts/directives/skychart.js
@@ -3,10 +3,44 @@
 angular.module('myExpenseKeeperApp')
   .directive('skyChart', function () {
     return {
-      template: '<div></div>',
       restrict: 'AE',
       link: function postLink(scope, element, attrs) {
-        element.text('this is the skychart directive');
+          element.highcharts({
+              chart: {
+                  plotBackgroundColor: null,
+                  plotBorderWidth: null,
+                  plotShadow: false
+              },
+              title: {
+                  text: 'My Expense Report'
+              },
+              plotOptions: {
+                  pie: {
+                      allowPointSelect: true,
+                      cursor: 'pointer',
+                      dataLabels: {
+                          enabled: true,
+                          color: '#000000',
+                          connectorColor: '#000000',
+                          formatter: function() {
+                              return '<b>'+ this.point.name +'</b>: '+ Highcharts.numberFormat(this.percentage, 2) +' %';
+                          }
+                      },
+                      tooltip: {
+                          pointFormat: '{series.name}: <b>{point.x}</b>',
+                          valueDecimals: 1
+                      }
+                  }
+              },
+              series: [{
+                  type: 'pie',
+                  name: 'Money spent',
+                  data: [
+                      {name: 'Computer', x: 200, y: 200},
+                      {name: 'Food', x: 100 , y: 100}
+                  ]
+              }]
+          });
       }
     };
   });
